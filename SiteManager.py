2386d494d595c37fdfd69aec44fd34d856baf211
Youngil Kim <youngilkim822@gmail.com>
2022-11-29 14:25:40 -0500

    some changes in main

---
+++ SiteManager.py
@@ -0,0 +1,12 @@
+import LockManager
+import TransactionManager
+import DataManager
+
+class SiteManager:
+
+
+    def __init__(self) -> None:
+        pass
+
+    
+    #def processPendingOperation(self, ts) :
